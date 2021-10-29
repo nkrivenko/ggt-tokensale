564955b990bdf708795f42d1f31897042d3f2357
Nikolai Krivenko <nkrivenko35@gmail.com>
2021-10-30 01:02:20 +0300

    [#3] Add token to contract

--- contracts/mocks/StubERC20Token.sol
+++ contracts/mocks/StubERC20Token.sol
@@ -1,9 +1,14 @@
 pragma solidity 0.5.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
 
 /* solhint-disable no-empty-blocks */
-contract StubERC20Token is ERC20 {
+contract StubERC20Token is ERC20Detailed, ERC20Mintable {
+    uint8 internal constant DECIMALS = 18;
 
+    constructor () public ERC20Detailed("GGT", "Godji Game Token", DECIMALS) {
+        
+    }
 }
---
+++ test/GGTToken.test.js
@@ -0,0 +1,99 @@
+const BN = web3.utils.BN;
+
+const GGTErc20Token = artifacts.require('GGTToken');
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bn')(BN))
+  .should();
+
+const { singletons, expectRevert, ether } = require('@openzeppelin/test-helpers');
+
+contract('GGTToken', function([ funder, owner, minter, user ]) {
+
+	const NAME = "Godji Game Token";
+	const SYMBOL = "GGT";
+    const DECIMALS = new BN(18);
+
+    const TOKENS_TO_MINT = ether("1");
+    const CAP = ether("50000000");
+
+	beforeEach(async function() {
+		this.token = await GGTErc20Token.new(NAME, SYMBOL, CAP, owner);
+	});
+
+	it('should create token with parameters provided', async function() {
+		this.token.should.exist;
+
+		NAME.should.be.equal(await this.token.name());
+		SYMBOL.should.be.equal(await this.token.symbol());
+        DECIMALS.should.be.bignumber.equal(await this.token.decimals());
+        CAP.should.be.bignumber.equal(await this.token.cap());
+        owner.should.be.equal(await this.token.getOwner());
+	});
+
+    describe('should mint tokens', function() {
+        it('should issue tokens if `mint` is called by MINTER', async function() {
+            await this.token.addMinter(minter, {from: owner});
+            await this.token.mint(user, TOKENS_TO_MINT, {from: minter});
+    
+            (await this.token.balanceOf(user)).should.be.bignumber.equal(TOKENS_TO_MINT);
+        });
+
+        it('should issue tokens if `mint` is called by owner', async function() {
+            await this.token.mint(user, TOKENS_TO_MINT, {from: owner});
+    
+            (await this.token.balanceOf(user)).should.be.bignumber.equal(TOKENS_TO_MINT);
+        });
+    
+        it('should revert if `mint` is called not by MINTER or owner', async function() {
+            await expectRevert(this.token.mint(user, TOKENS_TO_MINT, {from: user}), "GGTToken: only MINTER or owner can call this method");
+        });
+
+        it('should mint tokens if totalSupply will not exceed the cap', async function() {
+            await this.token.mint(user, CAP, {from: owner}).should.be.fulfilled;
+
+            CAP.should.be.bignumber.equal(await this.token.balanceOf(user));
+        });
+
+        it('should not mint tokens if totalSupply will exceed the cap', async function() {
+            await expectRevert(this.token.mint(user, CAP.add(new BN(1)), {from: owner}), "ERC20Capped: cap exceeded");
+        });
+    });
+
+    describe('should provide the access control to owner of contract', function() {
+
+        it('should grant MINTER role to the address if called by owner', async function() {
+            await this.token.addMinter(user, {from: owner});
+
+            (await this.token.isMinter(user)).should.be.true;
+        });
+
+        it('should revoke MINTER role to the address if called by owner', async function() {
+            await this.token.addMinter(user, {from: owner});
+            await this.token.removeMinter(user, {from: owner});
+
+            (await this.token.isMinter(user)).should.be.false;
+        });
+
+        it('should not grant MINTER role to the address if called not by owner', async function() {
+            await expectRevert(this.token.addMinter(user, {from: user}), "GGTToken: caller is not the owner");
+        });
+
+        it('should not revoke MINTER role to the address if called not by owner', async function() {
+            await this.token.addMinter(user, {from: owner});
+
+            await expectRevert(this.token.removeMinter(user, {from: user}), "GGTToken: caller is not the owner");
+        });
+
+        it('should revert when trying to call addMinter with minter address', async function() {
+            await this.token.addMinter(user, {from: owner});
+
+            await expectRevert(this.token.addMinter(user, {from: owner}), "GGTToken: Specified address is already a minter");
+        });
+
+        it('should revert when trying to call removeMinter with non-minter address', async function() {
+            await expectRevert(this.token.removeMinter(user, {from: owner}), "GGTToken: Specified address is not a minter");
+        });
+    });
+});
